81f597c79c4795966072695c786da69b674b92da
Apurva Wadekar <apurva.w17@gmail.com>
2021-12-30 20:23:47 +0530

    base created

---
+++ react-css/src/apolloClient/client.ts
@@ -0,0 +1,16 @@
+import {
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
+// import { RestLink } from "apollo-link-rest";
+
+const httpLink = new HttpLink({
+  uri: "https://api.graphql.jobs/",
+});
+
+export const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: ApolloLink.from([httpLink]),
+});
